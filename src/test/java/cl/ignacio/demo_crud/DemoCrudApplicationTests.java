378eaeedc9df98e50bb6ff441a7f7665cb3d7cd5
Ignacio Silva Silva <isilva@seiza-ti.cl>
2021-03-15 09:43:52 -0300

    Initial commit

---
+++ src/test/java/cl/ignacio/demo_crud/DemoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ignacio.demo_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
